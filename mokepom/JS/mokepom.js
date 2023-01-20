b63af38b88a677c5def909d3cca4f2654c7a42c8
Leonardo Lenis <leonardo.lenis@masglobalconsulting.com>
2023-01-20 16:37:06 -0500

    Seleccionar mascota enemigo

--- mokepom/JS/mokepom.js
+++ mokepom/JS/mokepom.js
@@ -1,20 +1,48 @@
+let ataqueJugador
+
 function iniciarJuego() {
   let botonMascotaJugador = document.getElementById("boton-mascota");
   botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+
+  let botonFuego
+  let botonTierra
+  let botonAgua
 }
 
 function seleccionarMascotaJugador() {
   let inputHipodoge = document.getElementById("hipodoge");
   let inputCapipepo = document.getElementById("capipepo");
   let inputRatigueya = document.getElementById("ratigueya");
+  let spanMascotaJugador = document.getElementById("mascota-jugador");
 
   if (inputHipodoge.checked) {
-    alert("Seleccionaste Hipodoge");
-  } else if(inputCapipepo.checked){
-    alert('Seleccionaste Capipepo')
+    spanMascotaJugador.innerHTML = "Hipodoge";
+  } else if (inputCapipepo.checked) {
+    spanMascotaJugador.innerHTML = "Capipepo";
   } else if (inputRatigueya.checked) {
-    alert('Seleccionaste Ratigueya')
+    spanMascotaJugador.innerHTML = "Ratigueya";
+  } else {
+    alert("Selecciona una mascota");
   }
+
+  seleccionarMascotaEnemigo();
+}
+
+function seleccionarMascotaEnemigo() {
+  let ataqueAleatorio = aleatorio(1, 3);
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+
+  if (ataqueAleatorio == 1) {
+    spanMascotaEnemigo.innerHTML = "Hipodoge"
+  } else if (ataqueAleatorio == 2) {
+    spanMascotaEnemigo.innerHTML = "Capipepo"
+  } else {
+    spanMascotaEnemigo.innerHTML = "Ratigueya"
+  }
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener("load", iniciarJuego);
